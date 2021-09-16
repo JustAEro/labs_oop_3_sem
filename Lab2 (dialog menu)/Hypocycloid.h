c6103cef7464d758a79327aaad4d3c0d6496afaa
ilya_polukhin <71003158+JustAEro@users.noreply.github.com>
2021-09-16 03:00:11 +0300

    Add files via upload

---
+++ Lab2 (dialog menu)/Hypocycloid.h
@@ -0,0 +1,50 @@
+#pragma once
+
+
+const double eps = 10e-5;
+
+struct Point
+{
+	double x;
+	double y;
+};
+
+class Hypocycloid
+{
+public:
+	Hypocycloid();
+	Hypocycloid(double radBig, double radSmall, double dist = 1.0);
+
+	//setters
+	Hypocycloid& setRadiusBig(double r);
+	Hypocycloid& setRadiusSmall(double r);
+	Hypocycloid& setDistance(double dist);
+		
+	//getters
+	double getRadiusBig() const { return radiusBig; }
+	double getRadiusSmall() const { return radiusSmall; }
+	double getDistance() const { return distance; }
+
+	//other methods
+	Point pointOfAngle(double t) const;
+	double curvRadiusOfAngle(double t) const;
+	double sectorialArea(double t) const;
+
+	enum class Types
+	{
+		SIMPLE,
+		LONG,
+		SHORT
+	};
+
+	Types type() const;
+
+		
+
+private:
+	double radiusBig;   //big circle
+	double radiusSmall;   //small circle
+	double distance;   //distance from center of small circle to rolling point
+};
+
+
