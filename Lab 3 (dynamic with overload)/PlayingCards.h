ddf9e9486464b3995c53c5b35947d10307151ecd
ilya_polukhin <71003158+JustAEro@users.noreply.github.com>
2021-10-03 20:20:05 +0300

    Add files via upload

---
+++ Lab 3 (dynamic with overload)/PlayingCards.h
@@ -0,0 +1,82 @@
+#pragma once
+
+enum class Suits
+{
+	Diamonds,
+	Hearts,
+	Clubs,
+	Spades,
+
+	Count
+};
+
+enum class Ranks 
+{
+	Two, Three, Four, Five, Six, Seven, Eight, Nine, Ten, 
+	Jack, Queen, King, Ace,
+
+	Count
+};
+
+struct Card
+{
+	Suits suit;
+	Ranks rank;
+};
+
+
+Card generate_random_card();
+
+
+bool operator== (const Card& card_1, const Card& card_2);
+
+std::wostream& operator<< (std::wostream& wos, const Suits& suit);
+std::wostream& operator<< (std::wostream& wos, const Ranks& rank);
+
+std::wostream& operator<< (std::wostream& wos, const Card& card);
+
+
+class PlayingCards
+{
+public:
+	PlayingCards();
+	explicit PlayingCards(int count);
+	explicit PlayingCards(Card card_init);
+	PlayingCards(const PlayingCards& playing_cards);
+
+	~PlayingCards();
+
+	//getters
+	size_t getMaxSize() const noexcept { return MAX_SIZE; }
+	size_t getCurrentCount() const noexcept { return current_count; }
+	
+	PlayingCards& addNewRandomCard();
+
+	PlayingCards& sort();
+
+	int findCard(const Card& card) const;
+
+	PlayingCards subGroupOfSameSuit(Suits suit) const noexcept;
+	
+	PlayingCards& operator= (const PlayingCards& playing_cards);
+
+	const Card& operator[] (int i) const;
+
+
+	PlayingCards& operator+= (Card card);
+
+	PlayingCards& operator++();
+	const PlayingCards operator++(int);
+
+
+	friend std::wostream& operator<< (std::wostream& wos, const PlayingCards& playing_cards);
+
+
+private:
+	static const size_t MAX_SIZE = 52;
+	size_t current_count;
+	Card* cards;
+
+	void copy_deck(const Card* src, Card* dest, size_t len) const;
+};
+
